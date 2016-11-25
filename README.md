78537e77401a0dadf11becd26596881a01824bde
aM3z <amezolma@plu.edu>
2016-11-25 14:26:04 -0800

    add error checking to read_clue

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ This is an abstract algebra project involving various cryptosystems such as shif
 
 *Note:* 
 Line 1 and line 2 must contain valid R expressions. For example, if a=2 and b=5 then line 1 and line 2 must contain integers 2 and 5, respectively.
-If line 1 represents a matrix then it should have the following format "c; d" where c is a numerical vector c(1,2,3,4) containing the matrix entries and d is an integer indicating the number of matrix columns.
+If line 1 represents a matrix then it should be a square matrix and line 1 should contain a numerical vector c(1,2,3,4) of length equal to a square number containing the by-column entries.
 If line 2 represents a vector then it should contain a numerical vector c(1,2) with length equal to the number of columns specified in line 1.
--- crypto/R/read_clue.R
+++ crypto/R/read_clue.R
@@ -9,22 +9,33 @@ read_clue<-function(filepath) {
 	# read file line-by-line
 	line<-readLines(filepath)
 	# split first line
-	first_line_split<-unlist(strsplit(line[1], ";"))
+	#first_line_split<-unlist(strsplit(line[1], ";"))
 	# eval first line
-	if(length(first_line_split) == 1) {
-		a<-eval(parse(text=line[1]))	
-	} else if (length(first_line_split) == 2) {
-		entries<-eval(parse(text=first_line_split[1]))
-		dim<-eval(parse(text=first_line_split[2]))
-		a<-matrix(entries, ncol=dim)
-	} else {
-		print("Line 1 is not formatted properly")
+	#if(length(first_line_split) == 1) {
+	#	a<-eval(parse(text=line[1]))	
+	#} else if (length(first_line_split) == 2) {
+	#	entries<-eval(parse(text=first_line_split[1]))
+	#	dim<-eval(parse(text=first_line_split[2]))
+	#	a<-matrix(entries, ncol=dim)
+	#} else {
+	#	stop("line 1 is not formatted properly")
+	#}
+
+	a<-eval(parse(text=line[1]))
+	if(length(a) > 1) {
+		dim<-sqrt(length(a))
+		a<-matrix(a, dim)
 	}
 	
 	# eval second line
 	b<-eval(parse(text=line[2]))
+	if(length(b) > 1 && length(b) != dim(a)[2]) stop ("b must have length equal to number of columns in the a matrix")
+	
 	# remove all white spaces from third line
 	c_text<-gsub("[[:blank:]]", "", line[3])
+
+	if(is.numeric(a) == F || is.numeric(b) == F) stop("a and b must be numeric")
+
 	# consolidate into data vector
 	data<-list(a=a, b=b, c_text=c_text)
 
