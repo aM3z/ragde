576d7dd58a76949f993fbc4ddea6dcfd81aebb91
aM3z <amezolma@plu.edu>
2016-11-25 19:52:53 -0800

    change c_text to cyphertext

--- crypto/R/decrypt_affine.R
+++ crypto/R/decrypt_affine.R
@@ -5,30 +5,40 @@
 #' @examples
 #' decode_affine(data)
 
-decode_affine<-function(data) {
+decrypt_affine<-function(data) {
+
 	# get a, b, and cyphetext from data	
 	a<-data[["a"]]
 	b<-data[["b"]]
-	cyphertext<-data[["c_text"]]
+	cyphertext<-data[["cyphertext"]]
+
 	# this vector will be used to convert chars to digits
 	chars_to_digits<-0:25
 	names(chars_to_digits)<-LETTERS
+
 	# this vector will be used to convert digits to chars
 	digits_to_chars<-LETTERS
 	names(digits_to_chars)<-0:25
+
 	# define mod
 	mod<-length(chars_to_digits)
+
 	# convert cyphertext to a character vector
 	cypher_chars<-substring(cyphertext, seq(1, nchar(cyphertext), 1), seq(1, nchar(cyphertext), 1))
+
 	# convert character vector to numerical vector
 	cypher_digits<-chars_to_digits[cypher_chars]
+
 	# compute inverses for a and b
 	a_inverse<-crypto::mul_inv(a, mod)
 	b_inverse<-crypto::add_inv(b, mod)
+
 	# use decoding function
 	clear_digits<-crypto::mul_mod_n(a_inverse, crypto::add_mod_n(cypher_digits, b_inverse, mod), mod)
+
 	# convert clear digits to chars
 	clear_char<-digits_to_chars[as.character(clear_digits)]
+
 	# paste chars into a string
 	cleartext<-paste(clear_char, collapse="")
 
--- crypto/R/read_clue.R
+++ crypto/R/read_clue.R
@@ -6,21 +6,11 @@
 #' read_clue("../clues/filename.clue")
 
 read_clue<-function(filepath) {
+
 	# read file line-by-line
 	line<-readLines(filepath)
-	# split first line
-	#first_line_split<-unlist(strsplit(line[1], ";"))
-	# eval first line
-	#if(length(first_line_split) == 1) {
-	#	a<-eval(parse(text=line[1]))	
-	#} else if (length(first_line_split) == 2) {
-	#	entries<-eval(parse(text=first_line_split[1]))
-	#	dim<-eval(parse(text=first_line_split[2]))
-	#	a<-matrix(entries, ncol=dim)
-	#} else {
-	#	stop("line 1 is not formatted properly")
-	#}
 
+	# eval first line
 	a<-eval(parse(text=line[1]))
 	if(length(a) > 1) {
 		dim<-sqrt(length(a))
@@ -37,7 +27,7 @@ read_clue<-function(filepath) {
 	if(is.numeric(a) == F || is.numeric(b) == F) stop("a and b must be numeric")
 
 	# consolidate into data vector
-	data<-list(a=a, b=b, c_text=c_text)
+	data<-list(a=a, b=b, cyphertext=c_text)
 
 	return(data)
 }
